7fc3e2b3c0f7c56d92ccab28a4f08691fe1d189e
FranklysG <tunele095@gmail.com>
2023-11-25 16:36:32 -0300

    feature implements api connection

---
+++ api.js
@@ -0,0 +1,17 @@
+const base_url = 'https://app.salesdock.nl/api/testomgeving/v1/user/';
+const token = 'MXibGmyJlNNaCFxJVirLbQLvji8MngOuNiYHVOM9Qhe1FRS6yjfY6navgNcR'
+
+var myHeaders = new Headers();
+myHeaders.append("Accept", "application/json");
+myHeaders.append("Authorization", `Bearer ${token}`);
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("https://app.salesdock.nl/api/testomgeving/v1/user/products/428/energy?postcode=7521WC&housenumber=15&type=e_g&tarifftype=1&usage_e_high=300&usage_e_low=200&usage_g=500&has_return=1&return_e_high=100&return_e_low=50&low_tariff_time=t3&building_function=1&e_connection_type=E_3X35A&g_connection_type=G_G6_500_4000", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
