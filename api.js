050c0de7a7240beeb3d0046f11440f9a6d37d109
FranklysG <tunele095@gmail.com>
2023-11-29 23:53:52 -0300

    fix: change the base url with doamin and scope

--- api.js
+++ api.js
@@ -1,17 +1,33 @@
-const base_url = 'https://app.salesdock.nl/api/hosted-energy/v1';
-const token = 'MXibGmyJlNNaCFxJVirLbQLvji8MngOuNiYHVOM9Qhe1FRS6yjfY6navgNcR'
+const domain = "hosted-energy";
+const scope = "user";
+const version = "v1";
+const base_url = `https://app.salesdock.nl/api/${domain}/${version}/${scope}`;
+const token = "MXibGmyJlNNaCFxJVirLbQLvji8MngOuNiYHVOM9Qhe1FRS6yjfY6navgNcR";
 
 var myHeaders = new Headers();
 myHeaders.append("Accept", "application/json");
 myHeaders.append("Authorization", `Bearer ${token}`);
 
 var requestOptions = {
-  method: 'GET',
+  method: "GET",
   headers: myHeaders,
-  redirect: 'follow'
+  redirect: "follow",
 };
 
-fetch(`${base_url}/user/products/428/energy?postcode=7521WC&housenumber=15&type=e_g&tarifftype=1&usage_e_high=300&usage_e_low=200&usage_g=500&has_return=1&return_e_high=100&return_e_low=50&low_tariff_time=t3&building_function=1&e_connection_type=E_3X35A&g_connection_type=G_G6_500_4000`, requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+// get the user
+fetch(`${base_url}`, requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.log("error", error));
+
+// get all forms
+fetch(`${base_url}/forms`, requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.log("error", error));
+
+// get a expecific form
+fetch(`${base_url}/forms/916/elements`, requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.log("error", error));
